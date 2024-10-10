dce37950c55c7bd3a174e0c4b83dfd92400f3b8d
Yongye Tan <yongyetan1209@gmail.com>
2024-10-10 18:59:29 -0400

    finished milestone 1

--- VerkadaSubmission/PokedexAppView.swift
+++ VerkadaSubmission/PokedexAppView.swift
@@ -11,14 +11,31 @@ struct PokedexAppView: View {
 
     @State private var pokedexViewModel = PokedexViewModel()
 
+    @State private var selectedPokemon: Pokemon? = nil
+
     let columns = Array(repeating: GridItem(.adaptive(minimum: 100, maximum: 200)), count: 3)
 
     var body: some View {
         NavigationStack {
             ScrollView {
+
+                VStack {
+                    if let selectedPokemon {
+                        AsyncImage(url: URL(string: selectedPokemon.sprites?.frontDefault ?? "")) { result in
+                            result.image?
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .frame(width: 200, height: 200)
+                    }
+                }
+
                 LazyVGrid(columns: columns) {
                     ForEach(pokedexViewModel.pokemons, id: \.self) { pokemon in
                         AsyncImage(url: URL(string: pokemon.sprites?.frontDefault ?? ""))
+                            .onTapGesture {
+                                selectedPokemon = pokemon
+                            }
                     }
                 }
             }
@@ -26,7 +43,6 @@ struct PokedexAppView: View {
             .task {
                 do {
                     try await pokedexViewModel.fetchPokemon()
-                    print(pokedexViewModel.pokemons)
                 } catch {
                     print(error)
                 }
