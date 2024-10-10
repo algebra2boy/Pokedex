031ebc3dd664080eeb006dcff2b502f2ef787257
Yongye Tan <yongyetan1209@gmail.com>
2024-10-10 17:53:34 -0400

    Initial Commit

---
+++ VerkadaSubmission/VerkadaSubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerkadaSubmissionApp.swift
+//  VerkadaSubmission
+//
+//  Created by Yongye on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VerkadaSubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
